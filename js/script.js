013d0797f8e55f5a9fa6f936b336d74f4f24903c
Jormmm <146562442+Jormmm@users.noreply.github.com>
2024-05-08 21:18:02 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+	const headerButton = document.querySelector('.header__button')
+	const header = document.querySelector('.header')
+
+	headerButton.addEventListener('click', function () {
+		header.classList.toggle('active-header')
+	})
+})
+
+
+
+function toggleStatus(element) {
+	if (element.textContent === 'active') {
+		element.textContent = 'deactive'
+		element.classList.remove('active')
+		element.classList.add('deactive')
+	} else {
+		element.textContent = 'active'
+		element.classList.remove('deactive')
+		element.classList.add('active')
+	}
+}
+
+
